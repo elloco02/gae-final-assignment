faf183e6cf58b504b96118157fc27f2186abb046
Tonka-de <rmcmaster05@gmail.com>
2025-06-29 02:55:01 +0200

    fix: bullet

--- neues-spiel/weapon/bullet.gd
+++ neues-spiel/weapon/bullet.gd
@@ -1,35 +1,28 @@
 class_name Bullet
-extends CharacterBody2D
+extends Area2D
 
-@export var hitbox_component: HitboxComponent
+@export var weapon_attack: WeaponAttack
+
 var current_pierce_count: int = 0
-var damage: float
-var max_pierce: int
-var speed: float
-var weapon_attack: WeaponAttack
+
 
 func _ready():
-	if hitbox_component:
-		hitbox_component.hit_enemy.connect(on_enemy_hit)
-	# get bullet stats
-	damage = weapon_attack.damage
-	max_pierce = weapon_attack.max_pierce
-	speed = weapon_attack.speed
-	print("Bullet speed: ", speed)
+	self.area_entered.connect(_on_area_entered)
+	print("Bullet speed: ", weapon_attack.speed)
+
 
 # move bullet in shooting direction and check collisions
 func _physics_process(delta: float) -> void:
 	var direction = Vector2.RIGHT.rotated(rotation)
-	velocity = direction * speed
-	var collider = move_and_collide(velocity * delta)
-	# collision with Enemy
-	if collider and collider is Enemy:
-		on_enemy_hit()
+	var velocity = direction * weapon_attack.speed
+	position += velocity * delta
+
 
 # when Enemy is hit
-func on_enemy_hit():
-	# increase current_pierce_count
+func _on_area_entered(area: Area2D):
+	if not area is HitboxComponent:
+		return
+
 	current_pierce_count += 1
-	# destroy bullet when max_piece reached
-	if current_pierce_count >= max_pierce:
+	if current_pierce_count >= weapon_attack.max_pierce:
 		queue_free()
